ba41f92e366914791fad891dc46d5335956fea6a
gregfowler85 <81462193+gregfowler85@users.noreply.github.com>
2026-03-02 20:24:41 +0000

    Add Terraform config to create DynamoDB table in LocalStack

---
+++ terraform/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    dynamodb = "http://localhost:4566"
+  }
+}
+
+resource "aws_dynamodb_table" "dynamotest" {
+  name         = "dynamotest"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "dynamotest"
+    Environment = "localstack"
+  }
+}
